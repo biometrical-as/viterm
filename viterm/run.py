9999d7059688af16c209f5a48aed45bd3456e5f6
martijho <martinhovin91@gmail.com>
2024-06-19 11:06:17 +0200

    commid e

--- viterm/run.py
+++ viterm/run.py
@@ -6,7 +6,6 @@ from typer import Argument, Option
 from viterm import Display, Preprocessor, canny, gray
 
 
-
 def get_preprocessor_function(preprocessors: List[Preprocessor]):
     preprocessors = [] if preprocessors is None else preprocessors
 
